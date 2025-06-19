ee55e743b3fc78941499fbb434baee559a802d98
Riccardo Gaist <riccardo.gaist@studenti.unimi.it>
2025-06-19 15:02:26 +0200

    Defined the custom row labels

--- r script/script esame.R
+++ r script/script esame.R
@@ -42,22 +42,68 @@ kable(table1, digits = 3, caption = "Table 1: Average fouls,
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
 # Start table 2 (poi sistema)
 
 
+# Supponendo che il tuo dataframe si chiami `df` e abbia queste variabili:
+# home, post_covid, fouls, yellows, reds, penalties
+
+# Helpers
+get_stars <- function(p) {
+  if (p < 0.01) return("***")
+  else if (p < 0.05) return("**")
+  else if (p < 0.10) return("*")
+  else return("")
+}
+
+get_gammas <- function(p) {
+  if (p < 0.01) return("γγγ")
+  else if (p < 0.05) return("γγ")
+  else if (p < 0.10) return("γ")
+  else return("")
+}
+
+# Crea una funzione per fare il lavoro per ogni variabile
+
+
+
+  
+  # Asterischi per ghost vs crowd (per home e away)
+  p_home <- t.test(df[df$home == 1 & df$post_covid == 0, ][[var]],
+                   df[df$home == 1 & df$post_covid == 1, ][[var]])$p.value
+  p_away <- t.test(df[df$home == 0 & df$post_covid == 0, ][[var]],
+                   df[df$home == 0 & df$post_covid == 1, ][[var]])$p.value
+  
+  # Gamma per away vs home (per crowd e ghost)
+  p_crowd <- t.test(df[df$home == 1 & df$post_covid == 0, ][[var]],
+                    df[df$home == 0 & df$post_covid == 0, ][[var]])$p.value
+  p_ghost <- t.test(df[df$home == 1 & df$post_covid == 1, ][[var]],
+                    df[df$home == 0 & df$post_covid == 1, ][[var]])$p.value
+  
+  # Formattazione
+  list(
+    Home_with = sprintf("%.3f", rows$home_crowd),
+    Home_ghost = sprintf("%.3f%s", rows$home_ghost, get_stars(p_home)),
+    Away_with = sprintf("%.3f%s", rows$away_crowd, get_gammas(p_crowd)),
+    Away_ghost = sprintf("%.3f%s%s", rows$away_ghost, get_gammas(p_ghost), get_stars(p_away))
+  )
+}
+
+# Applichiamo a ogni variabile
+table1 <- data.frame(
+  Variable = c("Fouls", "Yellow cards", "Red cards", "Penalty kicks"),
+  do.call(rbind, lapply(c("fouls", "yellows", "reds", "penalties"), build_table_row))
+)
+
+# Aggiungiamo righe per intestazioni
+table1_formatted <- rbind(
+  c("", "With crowds", "Ghost games", "With crowds", "Ghost games"),
+  c("Team", "Home", "Home", "Away", "Away"),
+  table1
+)
+
+# Stampiamo in console
+print(table1_formatted, row.names = FALSE, right = FALSE)
 
 
 
@@ -131,7 +177,15 @@ models_mfx <- list(
 )
 
 # Define custom row labels
-
+custom_row_labels <- c(
+  "home" = "Home team",
+  "post_covid" = "Crowd-less game",
+  "post_home" = "Crowd-less game * Home",
+  "var" = "VAR",
+  "home_var" = "Home team * VAR",
+  "trav_lt20m" = "Derby",
+  "ELO_diff" = "ELO difference"
+)
 
 # Generate the table using modelsummary
 msummary(
